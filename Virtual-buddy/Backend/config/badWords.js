bcf56f2a45f8e5103dfb150a04a775066a53b79c
Rajeswari Pakki <121120682+RajeswariPakki@users.noreply.github.com>
2025-12-23 10:28:36 +0530

    Add list of bad words for filtering

---
+++ Virtual-buddy/Backend/config/badWords.js
@@ -0,0 +1,15 @@
+export const badWords = [
+  "stupid", 
+  "idiot", 
+  "dumb", 
+  "shut up", 
+  "damn", 
+  "hell", 
+  "fuck", 
+  "shit", 
+  "bitch", 
+  "bastard",
+  "asshole",
+  "crap",
+  "sucks"
+];
